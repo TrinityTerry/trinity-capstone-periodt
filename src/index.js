c12ff221091534e73d061ce34b4a54cb81a8c552
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-03-19 23:59:08 -0500

    tried keys

--- src/index.js
+++ src/index.js
@@ -14,12 +14,26 @@ import "firebase/analytics";
 // Your web app's Firebase configuration
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!window.location.href.includes("localhost")) {
+  firebaseConfig = {
+    apiKey: process.env.PT_API_KEY,
+    authDomain: process.env.PT_AUTH_DOMAIN,
+    databaseURL: process.env.PT_DATABASE_URL,
+    projectId: process.env.PT_PROJECT_ID,
+    storageBucket: process.env.PT_STORAGE_BUCKET,
+    messagingSenderId: process.env.PT_MESSAGING_SENDER_ID,
+    appId: process.env.PT_APP_ID,
+    measurementId: process.env.PT_MEASUREMENT_ID
+  };
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.initializeApp(firebaseConfig);
+}
+
 firebase.analytics();
 
 ReactDOM.render(
   <BrowserRouter>
-       
     <App />
   </BrowserRouter>,
   document.getElementById("root")
