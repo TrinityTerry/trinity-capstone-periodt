548d3c9196fed33b26b8c2131fc1764d39c21415
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-03-20 13:34:39 -0500

    can see past cycles

--- src/design-library/calendar.js
+++ src/design-library/calendar.js
@@ -3,7 +3,6 @@ import PT_CALENDAR from "../components/calendar/PT_CALENDAR";
 import PT_TABLE from "../components/tables/PT_TABLE";
 import { Accordion, Icon } from "semantic-ui-react";
 
-
 const CalendarDescription = ({ history }) => {
   const [activeIndex, setActiveIndex] = useState();
 
@@ -16,8 +15,8 @@ const CalendarDescription = ({ history }) => {
   };
   return (
     <>
-    <hr/>
-         <Accordion styled>
+      <hr />
+      <Accordion styled>
         <Accordion.Title
           active={activeIndex === 0}
           index={0}
@@ -27,30 +26,30 @@ const CalendarDescription = ({ history }) => {
           Props
         </Accordion.Title>
         <Accordion.Content active={activeIndex === 0}>
-        <PT_TABLE
-          content={[
-            {
-              property: "date",
-              type: "string",
-              description: "'YYYY-MM' TO determine what month to create"
-            },
-            {
-              property: "highlight",
-              type: "array of number strings",
-              description: "Takes a string of numbers and those corrosponding days will be hilighted"
-            }
-          ]}
-        />
+          <PT_TABLE
+            content={[
+              {
+                property: "date",
+                type: "string",
+                description: "'YYYY-MM' TO determine what month to create"
+              },
+              {
+                property: "highlight",
+                type: "array of number strings",
+                description:
+                  "Takes a string of numbers and those corrosponding days will be hilighted"
+              }
+            ]}
+          />
         </Accordion.Content>
       </Accordion>
-    
 
       <pre>
         {`
           <PT_CALENDAR date={"2020-03"} highlight={["9", "10", "11"]}/>
           `}
       </pre>
-      <PT_CALENDAR date={"2020-03"} highlight={["9", "10", "11"]} />
+      <PT_CALENDAR date={"2020-03"} startPeriodDay={[9, 14]} endPeriodDay={[11, 16]} />
 
       <hr />
 
@@ -60,7 +59,7 @@ const CalendarDescription = ({ history }) => {
           `}
       </pre>
       <PT_CALENDAR date={"2020-02"} highlight={["9", "10", "11"]} />
-      
+
       <hr />
 
       <pre>
@@ -69,7 +68,7 @@ const CalendarDescription = ({ history }) => {
           `}
       </pre>
       <PT_CALENDAR date={"2020-05"} highlight={["9", "10", "11"]} />
-      
+
       <hr />
 
       <pre>
